dc02672818b759266782cdfd9cb0372c213ca2c7
John Duffell <john.duffell@guardian.co.uk>
2015-07-09 13:39:55 +0100

    share buttons and ice cream gif

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ function answer(text) {
 
 function why() {
     var why = document.createElement('a');
-    why.href = 'http://preview.gutools.co.uk/info/'
+    why.href = 'http://preview.gutools.co.uk/info/';
 
     var whyText = document.createTextNode("Why do we ask?");
     why.className = 'question__why';
@@ -67,15 +67,102 @@ function why() {
     return why;
 }
 
+function addManage(clazz) {
+    var wrap = document.createElement('div');
+    wrap.className = clazz;
+
+    var why = document.createElement('a');
+    why.href = 'http://preview.gutools.co.uk/info/';
+
+    var whyText = document.createTextNode("Undo");
+    why.className = 'question__onward';
+
+    why.appendChild(whyText);
+
+    var why2 = document.createElement('a');
+    why2.href = 'http://preview.gutools.co.uk/info/';
+
+    var whyText2 = document.createTextNode("Manage your data");
+    why2.className = 'question__onward';
+
+    why2.appendChild(whyText2);
+
+    wrap.appendChild(why);
+    wrap.appendChild(why2);
+    return wrap;
+}
+
+function addMembership(clazz) {
+    var wrap = document.createElement('div');
+    wrap.className = clazz;
+
+    var why = document.createElement('a');
+    why.href = 'http://membership.theguardian.com/';
+
+    var whyText = document.createTextNode("Help fund our work by becoming a member");
+    why.className = 'question__onward';
+
+    why.appendChild(whyText);
+
+    wrap.appendChild(why);
+    return wrap;
+}
+
+function addLogo(clazz, file) {
+    var wrap = document.createElement('div');
+    wrap.className = clazz;
+
+    var fb = document.createElement('img');
+    fb.className = 'question__onward';
+    fb.src = chrome.extension.getURL("FB.png");
+
+    var tw = document.createElement('img');
+    tw.className = 'question__onward';
+    tw.src = chrome.extension.getURL("TW.png");
+
+    wrap.appendChild(fb);
+    wrap.appendChild(tw);
+    return wrap;
+}
+
+function addIceCream(clazz, file) {
+    var wrap = document.createElement('div');
+    wrap.className = clazz;
+
+    //var fb = document.createElement('img');
+    //fb.className = 'question__icecream';
+    //fb.src = chrome.extension.getURL("icecream.jpeg");
+
+    var tw = document.createElement('img');
+    tw.className = 'question__icecream';
+    tw.src = chrome.extension.getURL("icecream.gif");
+
+    //wrap.appendChild(fb);
+    wrap.appendChild(tw);
+    return wrap;
+}
+
 var questionData = questions[id];
 
-if (questionData.type === 'marketing') {
+if (questionData.marketing) {
     questionDiv.appendChild(why());
 }
 questionDiv.appendChild(add('question__text', questionData.question));
 questionData.answers.forEach(function (answerText) {
     questionDiv.appendChild(answer(answerText));
 });
-questionDiv.appendChild(add('question__thanks js-thanks', 'Thanks for answering!'));
+if (questionData.marketing) {
+    questionDiv.appendChild(add('question__thanks js-thanks', 'That\'s crazy!  No-one could eat that much!'));
+    questionDiv.appendChild(addIceCream('question__thanks__wrapper js-thanks'));
+    questionDiv.appendChild(addManage('question__thanks__wrapper js-thanks'));
+} else {
+    if (questionData.membership) {
+        questionDiv.appendChild(add('question__thanks js-thanks', 'We appreciate your support.'));
+        questionDiv.appendChild(addMembership('question__thanks__wrapper js-thanks'));
+    } else {
+        questionDiv.appendChild(add('question__thanks js-thanks', '57% of readers agree with you, why not share your thoughts?'));
+        questionDiv.appendChild(addLogo('question__thanks__wrapper js-thanks'));
+    }
+}
 
 articleBody.appendChild(questionDiv);
--- questions.js
+++ questions.js
@@ -1,14 +1,28 @@
 var questions = {
-    "/music/2015/jul/06/damon-albarn-roskilde-carried-off-stage-following-five-hour-set-africa-express":
+    "/commentisfree/2015/jul/06/guardian-view-on-europe-after-greek-referendum-angela-merkel-must-take-lead":
     {
-        type: 'curated',
-        question: "What do you think?",
-        answers: ['Interesting', 'Boring']
+        marketing: false,
+        question: "Who do you think should take the lead?",
+        answers: ['Merkel', 'Tsipras'],
+        membership: false
     },
-    "/uk":
+    "/world/2015/jul/07/migrant-dies-uk-bound-freight-shuttle-near-calais-france":
     {
-        type: 'marketing',
+        marketing: false,
+        question: "Do you think this article was informative?",
+        answers: ['Yes', 'No'],
+        membership: true
+    },
+    "/stage/2015/jul/03/wonderland-review-manchester-international-festival-damon-albarn":
+    {
+        marketing: true,
         question: "Which gender are you?",
         answers: ['Male', 'Female']
+    },
+    "/technology/2014/jul/17/mit-students-3d-printing-ice-cream":
+    {
+        marketing: true,
+        question: "Have you ever eaten 20 scoops of ice cream in one day?",
+        answers: ['Yes!', 'No']
     }
 };
